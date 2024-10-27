544208dceb5abd031645f622802205f001c4c245
AshHydra <87881587+AshHydra@users.noreply.github.com>
2024-10-27 20:51:03 +0100

    ds

--- UTB_social_network_Dudik/obj/Debug/net8.0/UTB_social_network_Dudik.AssemblyInfo.cs
+++ UTB_social_network_Dudik/obj/Debug/net8.0/UTB_social_network_Dudik.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UTB_social_network_Dudik")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beb89d37f3d8aee6b1192e3eb2ba8685d93500c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("UTB_social_network_Dudik")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UTB_social_network_Dudik")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Utb_sc_Infrasctucture/obj/Debug/net8.0/Utb_sc_Infrasctucture.AssemblyInfo.cs
+++ Utb_sc_Infrasctucture/obj/Debug/net8.0/Utb_sc_Infrasctucture.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Utb_sc_Infrasctucture")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beb89d37f3d8aee6b1192e3eb2ba8685d93500c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Utb_sc_Infrasctucture")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Utb_sc_Infrasctucture")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
