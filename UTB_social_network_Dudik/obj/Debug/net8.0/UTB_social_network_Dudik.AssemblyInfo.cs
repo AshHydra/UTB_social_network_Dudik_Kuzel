088c1865a2937566b21f370b61146b36d7b06987
eeagle9 <j_kuzel@utb.cz>
2024-12-02 21:07:16 +0100

    hover barva buttonu login/register

--- UTB_social_network_Dudik/obj/Debug/net8.0/UTB_social_network_Dudik.AssemblyInfo.cs
+++ UTB_social_network_Dudik/obj/Debug/net8.0/UTB_social_network_Dudik.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UTB_social_network_Dudik")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a019680b6685d1b00b09b0bd39036b745b149b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b286429a97e3d60c4a9a793d324429b166a19fcf")]
 [assembly: System.Reflection.AssemblyProductAttribute("UTB_social_network_Dudik")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UTB_social_network_Dudik")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
