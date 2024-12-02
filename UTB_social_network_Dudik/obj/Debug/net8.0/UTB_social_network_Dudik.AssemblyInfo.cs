b286429a97e3d60c4a9a793d324429b166a19fcf
eeagle9 <j_kuzel@utb.cz>
2024-12-02 20:33:29 +0100

    mainpage css a html

--- UTB_social_network_Dudik/obj/Debug/net8.0/UTB_social_network_Dudik.AssemblyInfo.cs
+++ UTB_social_network_Dudik/obj/Debug/net8.0/UTB_social_network_Dudik.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UTB_social_network_Dudik")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+252a265dd23de92462ac4053d83bfae7d7b7f757")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a019680b6685d1b00b09b0bd39036b745b149b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("UTB_social_network_Dudik")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UTB_social_network_Dudik")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
