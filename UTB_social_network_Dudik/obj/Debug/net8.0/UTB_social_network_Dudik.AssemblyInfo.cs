c6de3f779bdaa87e6da41903e778579990785c54
Josef Kužel <119503297+eeagle9@users.noreply.github.com>
2025-01-13 16:41:29 +0100

    detaily

--- UTB_social_network_Dudik/obj/Debug/net8.0/UTB_social_network_Dudik.AssemblyInfo.cs
+++ UTB_social_network_Dudik/obj/Debug/net8.0/UTB_social_network_Dudik.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UTB_social_network_Dudik")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68f017f6c028b6a1efd8baec96058867baee0901")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ecf4d1e28005a7b5994b9b623c6fe617d73d333")]
 [assembly: System.Reflection.AssemblyProductAttribute("UTB_social_network_Dudik")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UTB_social_network_Dudik")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Utb_sc_Application/obj/Debug/net8.0/Utb_sc_Application.AssemblyInfo.cs
+++ Utb_sc_Application/obj/Debug/net8.0/Utb_sc_Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Utb_sc_Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68f017f6c028b6a1efd8baec96058867baee0901")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ecf4d1e28005a7b5994b9b623c6fe617d73d333")]
 [assembly: System.Reflection.AssemblyProductAttribute("Utb_sc_Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Utb_sc_Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
