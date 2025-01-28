3bc28caaf5c1a9f36dc90beb8e2cb9bb885eeeee
AshHydra <87881587+AshHydra@users.noreply.github.com>
2025-01-28 12:43:08 +0100

    ds

--- Utb_sc_domain/obj/Debug/net8.0/Utb_sc_domain.AssemblyInfo.cs
+++ Utb_sc_domain/obj/Debug/net8.0/Utb_sc_domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Utb_sc_Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63676fe3c2acf6a7a4ac10af3b422994d351d321")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+918fc6f5477814abf02050fa313312779f937d61")]
 [assembly: System.Reflection.AssemblyProductAttribute("Utb_sc_Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Utb_sc_Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
